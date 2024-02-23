b4a5d2fd9101cf67644a3e280be523eebfb8645d
Theo <theo.kgosiemang12@gmail.com>
2024-02-23 19:57:11 +0000

    Navbar and home page

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+import "../app.css";
+  import logo from "$lib/logo-placeholder-image.png"
+let showMenu = false
+function menu(){
+  showMenu = !showMenu
+}
+</script>
+<nav class="bg-gradient-to-r from-green-400 to-green-500">
+  <div class="md:flex md:justify-between">
+    <div class="flex justify-between">
+  <img src={logo} alt="Logo" class="h-20 " />
+ <svg class="md:hidden w-20 h-auto p-3 " on:click={menu} xmlns="http://www.w3.org/2000/svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none">
+<path d="M4 17H20M4 12H20M4 7H20" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+      </div>
+    <ul class="flex-col  md:flex md:flex-row md:items-center md:space-x-10 md:space-y-0 md:mt-0  {showMenu ? 'flex' : 'hidden' } p-5">
+  <li><a href="/" class="font-semibold hover:text-slate-300">HOME</a></li>
+    <li><a href="/about" class="font-semibold hover:text-slate-300">ABOUT</a></li>
+    <li><a href="/product" class="font-semibold hover:text-slate-300">PRODUCT</a></li>
+    <li><a href="/contact" class="font-semibold hover:text-slate-300">CONTACT</a></li>
+    <li><button class="{!showMenu ? '' : 'hidden'} p-3 bg-blue-300 rounded-md font-semibold hover:text-slate-300">Subscribe now</button></li>
+      </ul>
+    </div>
+</nav>
+
+
+<slot />
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+ <script> 
+ 
+</script>
+<div class="h-screen bg-[url('https://placehold.co/1400')]">
+
+  <span class="grid place-content-center w-full h-full text-center align-middle"> 
+    <h1 class="text-5xl md:text-8xl font-bold tracking-wide p-3"><span class="text-red-800">FIRE</span> ON ALL CYLINDERS</h1>
+     <p class="text-3xl p-3">subheading</p>
+   <button class="p-5 self-center md:px-8 font-bold font-sans text-lg rounded-3xl bg-green-600 max-w-[40%] justify-self-center mt-2">Get it now</button>
+  </span>
+</div>
