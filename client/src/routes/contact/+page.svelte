dc2eb0e57db5f4150a2546a2b8385737fa915dc9
Theo <theo.kgosiemang12@gmail.com>
2024-03-08 22:25:58 +0000

    minor fixes 2

--- client/src/routes/contact/+page.svelte
+++ client/src/routes/contact/+page.svelte
@@ -1,6 +1,6 @@
-<div class="">
+<div class="h-dvh">
 	<h1 class="p-5 text-center text-3xl font-bold sm:text-5xl">Contact US</h1>
-	<form action="#" class=" p-0 sm:mx-auto sm:w-[60%]">
+	<form action="#" class=" p-0 ml-3 sm:mx-auto sm:w-[60%]">
 		<label for="name" class="block font-semibold p-2">Name</label>
 		<input  id="name" class="block rounded border p-3 w-[90%] shadow-black shadow" placeholder="john"/>
 		<label for="email" class="block font-semibold p-2">Email</label>
